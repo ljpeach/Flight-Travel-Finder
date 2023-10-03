c14125c4641a8c8068459f56882f285958276f96
omes773 <olockhart88@gmail.com>
2023-10-03 12:33:22 -0500

    created event listener

---
+++ search.js
@@ -0,0 +1,37 @@
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    var startLocation = document.querySelector('#city-input').value;
+    var destination = document.querySelector("#city-input").value;
+    var startDate = document.querySelector('#start-date').value;
+    var returnDate = document.querySelector('#return-date').value;
+
+    if (!startLocation) {
+        console.error('You need to enter the city you are departing from!');
+        return;
+    }
+
+    if (!destination) {
+        console.error('You need to enter your destination!');
+        return;
+    }
+
+    if (!startDate) {
+        console.error('You need to enter the start date of your trip!');
+        return;
+    }
+
+    if (!returnDate) {
+        console.error('You need to enter the return date of your trip!');
+        return;
+    }
+
+
+    var queryString = './first-page.html?Departure=' + startLocation + '&destination=' + destination + '&startDate=' + startDate + '&returnDate=' + returnDate
+
+    location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
